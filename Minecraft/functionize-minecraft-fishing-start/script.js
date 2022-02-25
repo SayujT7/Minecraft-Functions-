6e94e166796f77b8b461bb28e32c2c3f932a6f24
SayujT7 <99748108+SayujT7@users.noreply.github.com>
2022-02-25 09:23:25 -0700

    Add files via upload

---
+++ Minecraft/functionize-minecraft-fishing-start/script.js
@@ -0,0 +1,84 @@
+// Minecraft Fishing Simulator by Mr. V
+
+// HTML Elements
+let steveImgEl = document.getElementById("steve-img");
+let alexImgEl = document.getElementById("alex-img");
+let villagerImgEl = document.getElementById("villager-img");
+let fishBtnEl = document.getElementById("fish-btn");
+let resultImgEl = document.getElementById("result-img");
+let codSpanEl = document.getElementById("cod-span");
+let salmonSpanEl = document.getElementById("salmon-span");
+let tropicalSpanEl = document.getElementById("tropical-span");
+let pufferSpanEl = document.getElementById("puffer-span");
+
+// Global Variables
+let character = "steve";
+let numCod = 0;
+let numSalmon = 0;
+let numTropical = 0;
+let numPuffer = 0;
+let randNum;
+
+// Fish Event
+fishBtnEl.addEventListener("click", catchFish);
+
+function catchFish() {
+  if (character === "steve") {
+    // STEVE PROBABILITIES: cod (70%), salmon (20%), tropical (5%), puffer (5%)
+    stimulateCatch(70, 90, 95);
+  } else if (character === "alex") {
+    // ALEX PROBABILITIES: cod (10%), salmon (10%), tropical (30%), puffer (50%)
+    stimulateCatch(10, 20, 50)
+  } else if (character === "villager") {
+    stimulateCatch(25,50,75);
+  }
+}
+
+function stimulateCatch(cod, salmon, tropical) {
+  randNum = Math.random();
+  if (randNum < cod / 100) {
+    numCod++;
+    codSpanEl.innerHTML = numCod;
+    resultImgEl.src = "img/Raw-Cod.png";
+  } else if (randNum < salmon / 100) {
+    numSalmon++;
+    salmonSpanEl.innerHTML = numSalmon;
+    resultImgEl.src = "img/Raw-Salmon.png";
+  } else if (randNum < tropical / 100) {
+    numTropical++;
+    tropicalSpanEl.innerHTML = numTropical;
+    resultImgEl.src = "img/Tropical-Fish.png";
+  } else {
+    numPuffer++;
+    pufferSpanEl.innerHTML = numPuffer;
+    resultImgEl.src = "img/Pufferfish.png";
+  }
+}
+
+
+
+// Character Select
+steveImgEl.addEventListener("click", selectSteve);
+alexImgEl.addEventListener("click", selectAlex);
+villagerImgEl.addEventListener("click", selectVillager);
+
+function selectSteve() {
+  character = "steve"
+  steveImgEl.classList.add("active");
+  alexImgEl.classList.remove("active");
+  villagerImgEl.classList.remove("active");
+}
+
+function selectAlex() {
+  character = "alex";
+  steveImgEl.classList.remove("active");
+  alexImgEl.classList.add("active");
+  villagerImgEl.classList.remove("active");
+}
+
+function selectVillager() {
+  character = "villager";
+  steveImgEl.classList.remove("active");
+  alexImgEl.classList.remove("active");
+  villagerImgEl.classList.add("active");
+}
